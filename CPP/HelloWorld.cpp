a95fc1b7696ad0a4ab8676ddfafd74d38fe34618
Jon <jonholz@gmail.com>
2017-07-22 14:16:06 -0400

    Initial commit

---
+++ CPP/HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "Hello World CPP" << std::endl;
+   return 0;
+}
---
+++ HelloWorld.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+time C/HelloWorld
+time CPP/HelloWorld
+time Go/HelloWorld
+time java -cp Java HelloWorld
+time Perl/HelloWorld
+time Python/HelloWorld.py
+time Ruby/HelloWorld.rb
