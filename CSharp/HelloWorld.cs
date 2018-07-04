8c30aa4836274cddc16cd2bda4f85b94d2cabbd8
Jonathan Holzman <holz@mail.com>
2018-07-04 15:55:54 -0400

    added csharp code and added to script, modified gitignore for c, c++ and c# output files

---
+++ CSharp/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+   public class Hello
+   {
+      static public void Main()
+      {
+         Console.WriteLine("Hello World C#");
+      }
+   }
+}
+
--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 time C/HelloWorld
 time CPP/HelloWorld
+time mono CSharp/HelloWorld.exe
 time Go/HelloWorld
 time java -cp Java HelloWorld
 time Perl/HelloWorld
